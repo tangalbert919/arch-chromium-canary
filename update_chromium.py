c001b79d5b339496219db4b2f075bdb711cb67f5
Albert Tang <tangalbert919@yahoo.com>
2023-03-08 15:00:20 -0600

    Update script to use new version API
    Omaha is deprecated and is shutting down at the end of this month.

--- update_chromium.py
+++ update_chromium.py
@@ -2,8 +2,8 @@
 
 import json, urllib.request as request, os
 
-h1 = request.urlopen('https://omahaproxy.appspot.com/all.json?os=win').read().decode('utf-8')
+h1 = request.urlopen('https://versionhistory.googleapis.com/v1/chrome/platforms/win/channels/canary/versions').read().decode('utf-8')
 #print(h1)
 data = json.loads(h1)
-#print(data[0]['versions'][1]['version'])
-os.system(f"sed -i '7s/pkgver=[[:digit:]]\{{3\}}[[:punct:]][[:digit:]][[:punct:]][[:digit:]]\{{4\}}[[:punct:]][[:digit:]]/pkgver={data[0]['versions'][1]['version']}/g' PKGBUILD")
+#print(data['versions'][0]['version'])
+os.system(f"sed -i '7s/pkgver=[[:digit:]]\{{3\}}[[:punct:]][[:digit:]][[:punct:]][[:digit:]]\{{4\}}[[:punct:]][[:digit:]]/pkgver={data['versions'][0]['version']}/g' PKGBUILD")
